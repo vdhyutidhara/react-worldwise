d1743b7e8231fa5a75e3007e6fef4a41e7d8e8b2
vdhyutidhara <vinay.dhyutidhara@gmail.com>
2025-02-15 17:18:12 +0530

    Worked on homepage and AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
+import Login from './pages/Login';
 import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound'
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import AppNav from '../components/AppNav'
+import Map from '../components/Map';
+import Sidebar from '../components/Sidebar';
+import styles from './AppLayout.module.css';
 
 export default function AppLayout() {
   return (
-    <div>
-        <AppNav />
+    <div className={styles.app}>
+        <Sidebar />
+        <Map />
     </div>
   )
 }
